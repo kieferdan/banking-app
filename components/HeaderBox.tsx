d020c8aad18c9140585977fceeec1f68da70bfbf
Daniel Kiefer <danielfmpinho@gmail.com>
2024-07-18 00:40:28 -0300

    chore: Rename TotlaBalanceBoxProps to TotalBalanceBoxProps and update references

---
+++ components/HeaderBox.tsx
@@ -0,0 +1,27 @@
+import TotalBalanceBox from "./TotalBalanceBox";
+
+const HeaderBox = ({
+  type = "title",
+  title,
+  subtext,
+  user,
+}: HeaderBoxProps) => {
+  return (
+    <div className="header-box">
+      <h1 className="header-box-title">
+        {title}
+        {type === "greeting" && (
+          <span className="text-bankGradient">&nbsp;{user}</span>
+        )}
+      </h1>
+      <p className="header-box-subtext">{subtext}</p>
+      <TotalBalanceBox
+        accounts={[]}
+        totalBanks={1}
+        totalCurrentBalance={3428.9}
+      />
+    </div>
+  );
+};
+
+export default HeaderBox;
