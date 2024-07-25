299b3f6aa23dbe776b90960ed9a31da3799c2579
Daniel Kiefer <danielfmpinho@gmail.com>
2024-07-25 00:03:49 -0300

    refactor: Update BankCard component to include Copy component for sharing account ID
    The BankCard component was refactored to include the Copy component, allowing users to easily share their account ID. This improves the user experience by providing a convenient way to copy the account ID to the clipboard.

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -2,20 +2,23 @@ import { formatAmount } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Copy from "./Copy";
 
 const BankCard = ({
   account,
   userName,
   showBalance = true,
 }: CreditCardProps) => {
+  console.log(account);
   return (
     <div className="flex flex-col">
-      <Link href="/" className="bank-card">
+      <Link
+        href={`/transaction-history/?id=${account.appwriteItemId}`}
+        className="bank-card"
+      >
         <div className="bank-card_content">
           <div>
-            <h1 className="text-16 font-semibold text-white">
-              {account.name || userName}
-            </h1>
+            <h1 className="text-16 font-semibold text-white">{account.name}</h1>
             <p className="font-ibm-plex-serif font-black text-white">
               {formatAmount(account.currentBalance)}
             </p>
@@ -24,10 +27,10 @@ const BankCard = ({
           <article className="flex flex-col gap-2">
             <div className="flex justify-between">
               <h1 className="text-12 font-semibold text-white">{userName}</h1>
-              <h2 className="text-12 font-semibold text-white">●●/●●</h2>
+              <h2 className="text-12 font-semibold text-white">●● / ●●</h2>
             </div>
             <p className="text-14 font-semibold tracking-[1.1px] text-white">
-              ●●●● ●●●● ●●●● <span className="text-16">4321</span>
+              ●●●● ●●●● ●●●● <span className="text-16">{account?.mask}</span>
             </p>
           </article>
         </div>
@@ -51,6 +54,8 @@ const BankCard = ({
           className="absolute top-0 left-0"
         />
       </Link>
+
+      {showBalance && <Copy title={account?.sharableId} />}
     </div>
   );
 };
